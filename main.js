92d78e4d734b185e95c88dbed1cbf31e25abcb28
VxC42 <valeriechin@gmail.com>
2020-08-08 01:21:51 -0700

    insert ts

--- main.js
+++ main.js
@@ -39,21 +39,14 @@ app.get('/',function(req, res, next){
     });
 });
 
-app.post('/insert', function(req,res,next){
-    var req = new XMLHttpRequest();
-    console.log(req.params.name)
-})
-
-/*app.post('/insert', function(req, res, next){
+app.post('/insert', function(req, res, next){
     var context = {};
-    if(req.query['Add Workout']){
-        mysql.pool.query('INSERT INTO workouts(`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?, ?, ?, ?, ?)', [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
-            if(err){
-                next(err);
-                return;
-            };
-        });
-    };*/
+    mysql.pool.query('INSERT INTO workouts(`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?, ?, ?, ?, ?)', [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+        if(err){
+            next(err);
+            return;
+        };
+    });
     /*if(req.query['Delete']){
         mysql.pool.query('DELETE FROM workouts WHERE id = ?', [req.query.id], function(err, result){
             if(err){
@@ -61,7 +54,7 @@ app.post('/insert', function(req,res,next){
                 return;
             }
         });
-    }*//*
+    }*/
     mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
         if(err){
            next(err);
@@ -71,7 +64,7 @@ app.post('/insert', function(req,res,next){
     res.render('DBchart',context);
     });
 })
-*/
+
 
 app.use(function(req,res){
   res.status(404);
