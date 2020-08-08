8c734b6380c4a1fe1b414f30adf7d10aa4b8089e
VxC42 <valeriechin@gmail.com>
2020-08-07 23:59:57 -0700

    insert ts

--- main.js
+++ main.js
@@ -40,9 +40,8 @@ app.get('/',function(req, res, next){
 });
 
 app.post('/', function(req, res, next){
-    console.log('hi')
     var context={};
-    if(req.body['Add Workout']){
+    if(req.body['insert']){
         mysql.pool.query('INSERT INTO workouts(`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?, ?, ?, ?, ?)', [req.body.name, req.body.reps, req.body.weight, req.body.date. req.body.lbs], function(err, result){
             if(err){
                 next(err);
